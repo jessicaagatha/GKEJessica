b6beee5983e297bca6619df3ecd6e209e3960d56
jessicaagatha <149646572+jessicaagatha@users.noreply.github.com>
2023-11-07 14:32:41 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Nama: Jessica Agatha Cesaria Yap \n Nim: 51022009 \n Jurusan: Sistem Informasi \'n)
+    res.send('Nama: Jessica Agatha Cesaria Yap \n')
+    res.send('Nim: 51022009 \n')
+    res.send('Jurusan: Sistem Informasi \n')
 })
 
 app.listen(PORT, HOST)
